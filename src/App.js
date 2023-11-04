0b08a63849540b280a4c83aa1d4c937072f3fa53
Ayoub-45 <ayoubnasraoui161@yahoo.fr>
2023-11-04 08:00:45 +0100

    Leverage the Navigation bar component

--- src/App.js
+++ src/App.js
@@ -1,14 +1,29 @@
-import { Route, Routes } from "react-router-dom";
+import { Outlet, Route, Routes } from "react-router-dom";
 import Home from "./routes/home/Home.component";
-function Shop(){
-  return <h1>I am a shop component</h1>
+function Shop() {
+  return (
+    <>
+      <div>
+        <h1>I am a shop component</h1>;
+      </div>
+    </>
+  );
+}
+function Navigation() {
+  return (
+    <div>
+      <h1>I am the navigation component</h1>
+      <Outlet />
+    </div>
+  );
 }
 export default function App() {
   return (
     <Routes>
-      <Route path="/home" element={<Home />} >
-       <Route path="/home/shop" element={<Shop />}/>
-        </Route>;
+      <Route path="/" element={<Navigation />}>
+        <Route index element={<Home />} />
+        <Route path="shop" element={<Shop />} />
+      </Route>
     </Routes>
   );
 }
