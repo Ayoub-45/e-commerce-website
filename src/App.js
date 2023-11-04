e79477150cd7ff2fffa7d42b2105269687fda4d2
Ayoub-45 <ayoubnasraoui161@yahoo.fr>
2023-11-04 07:41:25 +0100

    Introducing React router outlet

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import { Route, Routes } from "react-router-dom";
 import Home from "./routes/home/Home.component";
+function Shop(){
+  return <h1>I am a shop component</h1>
+}
 export default function App() {
   return (
     <Routes>
-      <Route path="/home" element={<Home />} />;
+      <Route path="/home" element={<Home />} >
+       <Route path="/home/shop" element={<Shop />}/>
+        </Route>;
     </Routes>
   );
 }
