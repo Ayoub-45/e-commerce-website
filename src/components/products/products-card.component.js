a677237f66a12a260e9f79bd272e8f78d9481ef2
Ayoub-45 <ayoubnasraoui161@yahoo.fr>
2023-11-20 14:36:22 +0100

    Style and create product card

---
+++ src/components/products/products-card.component.js
@@ -0,0 +1,15 @@
+import Button from "../button/button.component";
+
+export default function ProductCard({product}){
+const {name,imageUrl,price}=product;
+return (
+    <div className="products-container">
+        <img src={imageUrl} alt={name}/>
+        <div className="footer">
+            <span className="name">{name}</span>
+            <span className="price">{price}</span>
+        </div>
+        <Button buttonType="inverted">Add to card</Button>
+    </div>
+)
+}
--- src/routes/shop/shop.component.js
+++ src/routes/shop/shop.component.js
@@ -1,20 +1,17 @@
 import { useContext } from "react"
 import { ProductsContext } from "../../contexts/products.context"
+import ProductCard from "../../components/products/products-card.component";
+import "./shop.style.scss";
 export default function Shop(){
  const {products}=useContext(ProductsContext);
     return(
-       <div>
-       {
-        products.map(({id,name})=>{
-            return(
-                <div key={id}>
-                   <h1>
-                    {name}
-                    </h1> 
-                </div>
-            )
-        })
-       }
+       <div className="container">
+        {
+            products.map(product=>{
+
+               return <ProductCard key={product.id} product={product}/>
+            })
+        }
        </div>
     )
 }
