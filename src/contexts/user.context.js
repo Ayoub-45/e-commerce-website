d9058d2a807c2ad7c1ff398cf3c4b3781af36f98
Ayoub-45 <ayoubnasraoui161@yahoo.fr>
2023-12-10 16:45:50 +0100

    Adding a user reducer to our context

--- src/contexts/user.context.js
+++ src/contexts/user.context.js
@@ -1,23 +1,32 @@
-import { createContext, useState, useEffect } from "react";
+import { createContext, useState, useEffect, useReducer } from "react";
 import {
   createUserDocumentsFromAuth,
   onAuthStateChangeListener,
 } from "../utils/firebase.utils";
+import { USER_ACTiON_TYPES, userReducer } from "../reducers/user.reducer";
 //Creating the actual context
+const INITIAL_STATE = {
+  currentUser: null,
+};
 export const UserContext = createContext({
   currentUser: null,
   setCurrentUser: () => null,
 });
 export const Userprovider = ({ children }) => {
-  useEffect(function () { 
-    const unsubscribe=onAuthStateChangeListener(user=>{
-      if(user)
-        createUserDocumentsFromAuth(user)
-      setCurrentUser(user)
-    })
-    return unsubscribe
+  const [state, dispatch] = useReducer(userReducer, INITIAL_STATE);
+  const { currentUser } = state;
+  console.log(currentUser);
+  const setCurrentUser = (user) => {
+    dispatch({ type: USER_ACTiON_TYPES.SET_CURRENT_USER, payload: user });
+  };
+  useEffect(function () {
+    const unsubscribe = onAuthStateChangeListener((user) => {
+      if (user) createUserDocumentsFromAuth(user);
+      setCurrentUser(user);
+    });
+    return unsubscribe;
   }, []);
-  const [currentUser, setCurrentUser] = useState(null);
+  //const [currentUser, setCurrentUser] = useState(null);
   const value = {
     currentUser,
     setCurrentUser,
---
+++ src/reducers/user.reducer.js
@@ -0,0 +1,16 @@
+export const USER_ACTiON_TYPES = {
+  SET_CURRENT_USER: "SET_CURRENT_USER",
+};
+export const userReducer = (state, action) => {
+  const { type, payload } = action;
+  console.log(action);
+  switch (type) {
+    case "SET_CURRENT_USER":
+      return {
+        ...state,
+        currentUser: payload,
+      };
+    default:
+      throw new Error(`Unhandled type ${type} in userReducer`);
+  }
+};
